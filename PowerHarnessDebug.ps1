fd475227b152f0d5aff01e9e2551a968e3dc91ba
Brian Kelly <bekelly@gmail.com>
2025-09-08 09:30:17 -0400

    I don't think the extra remove modules were doing anything.  This debug harness isn't 100% effective right now.  Maybe 60%?

--- PowerHarnessDebug.ps1
+++ PowerHarnessDebug.ps1
@@ -6,11 +6,6 @@ param (
 
 Write-Output "Resetting PowerHarness module"
 Remove-Module PowerHarness -Force -ErrorAction SilentlyContinue
-Remove-Module phLogger -Force -ErrorAction SilentlyContinue
-Remove-Module phUtil -Force -ErrorAction SilentlyContinue
-Remove-Module phEmailer -Force -ErrorAction SilentlyContinue
-Remove-Module phSQL -Force -ErrorAction SilentlyContinue
-
 
 Write-Output "Launching script: $ScriptToRun"
 . $ScriptToRun
