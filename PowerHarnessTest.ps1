4eaafe4b9584b2e765ac5a2a31af1136b4c8f6c9
Brian Kelly <bekelly@gmail.com>
2025-09-14 10:10:18 -0400

    Improved configurable test script

--- PowerHarnessTest.ps1
+++ PowerHarnessTest.ps1
@@ -6,23 +6,35 @@ Import-Module "$PSScriptRoot\PowerHarness\PowerHarness.psm1"
 $ph = Get-PowerHarness -scriptPath $MyInvocation.MyCommand.Path
 
 $ph.RunScript({
-	$ph.Logger.Info("Hello World from PowerHarnessTest!")
-    $ph.Logger.Debug("This log is not important.")
-    $ph.Logger.IndentIncrease()
-    $ph.Logger.Info("This is an indented info message.")
-    $ph.Logger.IndentIncrease()
-    $ph.Logger.Info("This is a more indented info message.`nIt's also a log statement with multiple lines.`nLine 3.`nLine 4.")
-    $ph.Logger.IndentDecrease()
-    $ph.Logger.Info("Back to previous indent level.")
-    $ph.Logger.IndentDecrease()
-    $ph.Logger.Error("This is a sample error message")
 
-    $ph.SQL.SetConnection($ph.Config.sqlConnection)
-    # $accountList = $ph.SQL.ExecReaderToHtmlTable("SELECT * FROM dbo.Account")
-    $accountList = $ph.SQL.ExecNonQuery("SELECT * FROM dbo.Account")
-    # $ph.Logger.Debug($accountList)
-    $body = $ph.emailer.ApplyDefaultTemplate($accountList)
-    $ph.Logger.Debug($body)
-    # Write-Host $body
-    # $ph.Emailer.Send("Test email from PowerHarness", $body, $ph.Config.notifyEmail)
+    #----------------------------------------------------------------------------------------------
+    # logging test
+    #----------------------------------------------------------------------------------------------
+    if ($ph.Config.logTestEnabled) {
+        $ph.Logger.Info("Hello World from PowerHarnessTest!")
+        $ph.Logger.Debug("This log is not important.")
+        $ph.Logger.IndentIncrease()
+        $ph.Logger.Info("This is an indented info message.")
+        $ph.Logger.IndentIncrease()
+        $ph.Logger.Info("This is a more indented info message.`nIt's also a log statement with multiple lines.`nLine 3.`nLine 4.")
+        $ph.Logger.IndentDecrease()
+        $ph.Logger.Info("Back to previous indent level.")
+        $ph.Logger.IndentDecrease()
+        $ph.Logger.Error("This is a sample error message")
+    }
+
+    #----------------------------------------------------------------------------------------------
+    # sql test
+    #----------------------------------------------------------------------------------------------
+    if ($ph.Config.sqlTestEnabled) {
+        $ph.SQL.SetConnection($ph.Config.sqlConnection)
+        $accountList = $ph.SQL.ExecReaderToHtmlTable("SELECT * FROM dbo.Account")
+        $accountList = $ph.SQL.ExecNonQuery("SELECT * FROM dbo.Account")
+        $ph.Logger.Debug($accountList)
+        $body = $ph.emailer.ApplyDefaultTemplate($accountList)
+        $ph.Logger.Debug($body)
+        Write-Host $body
+        $ph.Emailer.Send("Test email from PowerHarness", $body, $ph.Config.notifyEmail)
+    }
+
 })
